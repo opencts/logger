98388e13135c9bd105e623d4e477611fb7369d59
asaje379 <salemaffa@gmail.com>
2023-03-01 12:08:11 +0100

    chore: create server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello world');
+import express, { Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (_, res: Response) => {
+  res.status(200).send('Hello');
+});
+
+app.listen(4000, () => {
+  console.log('Server is up!');
+});
